0b47cd53d8fee45c48178bdc76200019c903d931
Sherzod Karimov <karimovj@gmail.com>
2018-07-20 09:43:39 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,7 +3,22 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(arr)
+badges = []
  arr.each do |x|
-   badge_maker(x)
+   badges << badge_maker(x)
   end
+badges
+end 
+
+def assign_rooms(speakers)
+  rooms = []
+  speakers.each_with_index do |name, idx|
+    rooms << "Hello, #{name}! You'll be assigned to room #{idx + 1}!"
+  end
+  rooms
+end 
+
+def printer(attendees)
+  batch_badge_creator(attendees)
+  assign_rooms(attendees)
 end 
