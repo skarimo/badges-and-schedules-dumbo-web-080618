2950c218cc8a0a59adc9cfa06cbe0ae2d9d0cc11
Sherzod Karimov <karimovj@gmail.com>
2018-07-20 09:44:07 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -19,6 +19,6 @@ def assign_rooms(speakers)
 end 
 
 def printer(attendees)
-  batch_badge_creator(attendees)
-  assign_rooms(attendees)
+  puts batch_badge_creator(attendees)
+  puts assign_rooms(attendees)
 end 
